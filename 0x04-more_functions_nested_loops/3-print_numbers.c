cc484c8411c1dfd6f6c67c43225d0f0b9a2d1732
JBoina <boinajohn@gmail.com>
2023-03-24 02:55:34 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,8 @@
   *
   * Return: The numbers since 0 up to 9
   */
-void print_numbers(void)
+int main(void)
 {
-	int x;
-
-	for (x = 0; x <= 9; x++)
-	{
-	_putchar(x + '0');
-	}
-
-	_putchar('\n');
+	print_numbers();
+	return (0);
 }
