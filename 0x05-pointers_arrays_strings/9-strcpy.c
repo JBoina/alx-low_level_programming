c196b52e176149d0db0c451e4fff5b4f3d568929
JBoina <boinajohn@gmail.com>
2023-03-28 13:08:25 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,16 +6,25 @@
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
+ *
  * Return: the pointer to dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-	for (i = 0; src[1] != '\0'; i++)
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i++] = '\0';
+	dest[i] = '\0';
+
 	return (dest);
 }
