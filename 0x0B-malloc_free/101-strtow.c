d1a60da18dea406d3a7e39ee37d7c0fec97375f7
JBoina <boinajohn@gmail.com>
2023-04-12 10:59:46 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,16 +22,27 @@ int count_word(char *s)
 		
 		int flag, c, w;
 		
-			flag = 0;		
-		w = 0;
+
 		
-	for (c = 0; s[c] != '\0'; c++)
-{		
- if (s[c] == ' ')		
-flag = 0;		
-else if (flag == 0)
-{
-		flag = 1;
+			flag = 0;
+		
+				w = 0;
+		
+
+		
+					for (c = 0; s[c] != '\0'; c++)
+		
+							{
+		
+										if (s[c] == ' ')
+		
+														flag = 0;
+		
+												else if (flag == 0)
+		
+															{
+		
+flag = 1;
 		
 																						w++;
 		
@@ -85,7 +96,6 @@ char **strtow(char *str)
 		
 	if (matrix == NULL)
 		
-
 return (NULL);
 		
 
